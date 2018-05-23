f5a1de9db66b73abf898dd298ff35b216b70a100
Paul Lee <okokjae40@gmail.com>
2018-05-23 19:16:56 -0400

    EOD Commit

--- main.js
+++ main.js
@@ -1,14 +1,24 @@
 function multiply_text() {
 	var in_text = document.forms['myForm']['input_text'].value;
 	var count = document.forms['myForm']['multiply_count'].value;
+	var index_choice = document.querySelector('input[name="choice"]:checked').value;
 	var out = ""
 	
-	
-	for (i=0; i<count; i++){
-		out += in_text;
-		out += "\n";
+	if(index_choice == "no"){
+		for (i=0; i<count; i++){
+			out += in_text;
+			out += "\n";
+		}
+	}else{
+		for (i=0; i<count; i++){
+			out += in_text;
+			out += " ";
+			out += i;
+			out += "\n";
+		}
 	}
 	
+
 	textbox = document.getElementById('output');
 	textbox.value = out
 }
