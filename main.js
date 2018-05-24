b383f86c31bcbb338388c5922485a6e03e7f75f3
Paul Lee <okokjae40@gmail.com>
2018-05-23 23:58:54 -0400

    EOD Commit

--- main.js
+++ main.js
@@ -1,4 +1,10 @@
-function multiply_text() {
+function log(message){
+	/*Helper function to log to the console messages for debugging.*/
+	console.log(message);
+}
+
+function print_multiplied_text() {
+	/*This function will multiply the text you input into the input box.*/
 	var in_text = document.forms['myForm']['input_text'].value;
 	var count = document.forms['myForm']['multiply_count'].value;
 	var index_choice = document.querySelector('input[name="choice"]:checked').value;
@@ -20,5 +26,24 @@ function multiply_text() {
 	
 
 	textbox = document.getElementById('output');
-	textbox.value = out
+	textbox.value = out;
 }
+
+function remove_new_lines() {
+	/*This function will remove all new lines from the given input box.*/
+	var regex = new RegExp('\n', 'g');
+	var index_choice = document.querySelector('input[name="choice"]:checked').value;
+	textarea_input = document.getElementById("input").value;
+	
+	if(index_choice == "none"){
+		textarea_output = textarea_input.replace(regex, "");		
+	} else if (index_choice == "tab"){
+		textarea_output = textarea_input.replace(regex, "\t");
+	} else {
+		textarea_output = textarea_input.replace(regex, ",");	
+	}
+
+	textarea_out = document.getElementById('output');
+	textarea_out.value = textarea_output;
+}
+
